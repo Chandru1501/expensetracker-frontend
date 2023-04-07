c5133f502d6d0116e86132ac973e08bd31152b69
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-04-08 03:08:26 +0530

    backend responsive signup and login

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+var type = document.querySelector('#type');
+console.log(type.textContent);
+
+if(type.textContent==="SignUp") {
+
+//................................... SIGN-UP ....................................................
+
 let username = document.querySelector('#username');
 let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 let signup = document.querySelector('#btn');
 let userNotFound = document.querySelector('#userNotFound');
 
-signup.addEventListener('click',addUser);
-
-function addUser(){
+function addUser(event){
+    event.preventDefault();
 let myObj = {
     username : username.value,
     email : email.value,
@@ -26,5 +32,30 @@ async function postUser(myObj){
  else{
     location.replace("/Login.html")
  }
+}
 
+}
+
+else if (type.textContent==="Login"){
+
+//........................................... LOGIN ................................................................
+
+let userEmail = document.querySelector('#emailLogin');
+let userPassword = document.querySelector('#passwordLogin');
+
+
+function login(event){
+    event.preventDefault();
+  let myLogin = {
+    email : userEmail.value,
+    password : userPassword.value 
+  }
+console.log(myLogin);
+ userLogin(myLogin);
+}
+
+async function userLogin(myLogin){
+    let response = await axios.post('http://localhost:8080/admin/login',myLogin);
+    console.log(response);
+}
 }
